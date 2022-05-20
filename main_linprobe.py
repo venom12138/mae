65c561cd574ce9c637b8a16166a2649b77bee31d
venom12138 <1390852413@qq.com>
2022-05-20 18:09:28 +0000

    savecode

--- main_linprobe.py
+++ main_linprobe.py
@@ -260,8 +260,4 @@ def save_checkpoint(state, checkpoint='checkpoint', filename='checkpoint.pth.tar
     torch.save(state, filepath)
 
 if __name__ == '__main__':
-    args = get_args_parser()
-    args = args.parse_args()
-    if args.output_dir:
-        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
