a1457826dd47cf7e1d31391bd28c1f2caed15bf0
venom12138 <1390852413@qq.com>
2022-05-21 09:26:40 +0000

    savecode

--- main_pretrain.py
+++ main_pretrain.py
@@ -204,6 +204,7 @@ def main():
         if misc.is_main_process():
             exp.write(args.phase, train_metrics=train_metrics, epoch=epoch,lr=optimizer.param_groups[0]['lr'])
             total_time = time.time() - start_time
+    total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
