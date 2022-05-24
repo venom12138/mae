c366d995e061887a10fb09f1211378ba2a88bc50
venom12138 <1390852413@qq.com>
2022-05-24 12:33:49 +0000

    savecode

--- zombie.py
+++ zombie.py
@@ -6,4 +6,7 @@ a = torch.randn((34000,34000), device='cuda', dtype=torch.float64)
 
 while True:
     a += torch.mul(a,a.T)
-    # time.sleep(0.001)
+    time.sleep(0.3)
+    if exp_name = os.getenv('exp_name', default='zombie'):
+        if exp_name != 'zombie':
+            break
