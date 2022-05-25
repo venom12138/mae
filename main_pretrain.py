d90460982ac3ef7e1882cb9cf2fe39f88f5ed844
venom12138 <1390852413@qq.com>
2022-05-25 13:03:55 +0000

    savecode

--- main_pretrain.py
+++ main_pretrain.py
@@ -209,10 +209,15 @@ def main():
             args=args
         )
         
-        if (epoch + 1 == args.epochs or epoch%50==0) and misc.is_main_process():
-            misc.save_model(
-                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                loss_scaler=loss_scaler, epoch=epoch, exp=exp, isfinal=True)
+        if (epoch + 1 == args.epochs or epoch % 20==0) and misc.is_main_process():
+            if epoch + 1 == args.epochs:
+                misc.save_model(
+                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                    loss_scaler=loss_scaler, epoch=epoch, exp=exp, isfinal=True)
+            else:
+                misc.save_model(
+                    args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                    loss_scaler=loss_scaler, epoch=epoch, exp=exp)
 
         if misc.is_main_process():
             exp.write(args.phase, train_metrics=train_metrics, epoch=epoch)
